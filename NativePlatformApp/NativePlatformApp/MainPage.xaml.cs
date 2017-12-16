b5cad6901626301c2e779460ed4a52b06c3872b9
Jesus Angulo <jesus.angulo@outlook.com>
2017-12-15 21:35:17 -0500

    Adding initial layout

--- NativePlatformApp/NativePlatformApp/MainPage.xaml.cs
+++ NativePlatformApp/NativePlatformApp/MainPage.xaml.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.Net.Http;
-using ModernHttpClient;
-using Newtonsoft.Json;
-using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
 
 namespace NativePlatformApp
@@ -12,20 +7,17 @@ namespace NativePlatformApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage
     {
-        private readonly HttpClient _httpClient;
-
         public MainPage()
         {
             InitializeComponent();
-            _httpClient = new HttpClient(new NativeMessageHandler())
-            {
-                BaseAddress = new Uri("https://reqres.in/")
-            };
             this.BindingContext = this;
         }
 
-        public ObservableCollection<User> Users { get; } 
-            = new ObservableCollection<User>();
+        public ObservableCollection<object> Users { get; } 
+            = new ObservableCollection<object>()
+            {
+                new {avatar="icon.png",first_name="Jesus",last_name="Angulo"}
+            };
 
         public double Milliseconds
         {
@@ -39,33 +31,10 @@ namespace NativePlatformApp
             var stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            var json = await _httpClient.GetStringAsync("/api/users?page=1");
 
-            var users = JsonConvert.DeserializeObject<Response>(json).data;
-
-            users.ForEach(u=> Users.Add(u));
 
             stopwatch.Stop();
             Milliseconds = stopwatch.ElapsedMilliseconds;
         }
     }
-
-
-    public class Response
-    {
-        public int page { get; set; }
-        public int per_page { get; set; }
-        public int total { get; set; }
-        public int total_pages { get; set; }
-        public User[] data { get; set; }
-    }
-
-    public class User
-    {
-        public int id { get; set; }
-        public string first_name { get; set; }
-        public string last_name { get; set; }
-        public string avatar { get; set; }
-    }
-
 }
