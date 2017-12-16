2d993b59ab1ca7af28998db635e3a44ee16547d8
Jesus Angulo <jesus.angulo@outlook.com>
2017-12-15 22:16:37 -0500

    Basic mmvm

---
+++ NativePlatformApp/NativePlatformApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace NativePlatformApp.ViewModels
+{
+    public class MainViewModel : BindableObject
+    {
+        public ObservableCollection<object> Users { get; } = new ObservableCollection<object>()    
+        {
+            new {avatar="icon.png",first_name="Jesus",last_name="Angulo"}
+        };
+        
+        private double _milliseconds;
+        public double Milliseconds
+        {
+            get => _milliseconds;
+            set
+            {
+                _milliseconds = value;
+                OnPropertyChanged(nameof(Milliseconds));
+            }
+        }
+        
+        public ICommand DeleteCommand { get; }
+        public ICommand UpdateCommand { get; }
+
+        public MainViewModel()
+        {
+        }
+    }
+}
---
+++ NativePlatformApp/NativePlatformApp/Views/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using Xamarin.Forms.Xaml;
+
+namespace NativePlatformApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage
+    {
+        private readonly Stopwatch stopwatch;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.stopwatch = new Stopwatch();
+            this.BindingContext = this;
+        }
+
+        protected override async void OnAppearing()
+        {
+            stopwatch.Reset();
+            stopwatch.Start();
+
+
+
+            stopwatch.Stop();
+        }
+    }
+}
